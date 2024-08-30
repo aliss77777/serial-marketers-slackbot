ee247d13ecd7f17912e8fb96faf616f380a83aea
Alexander Liss <aliss77777@gmail.com>
2024-08-30 12:28:46 -0600

    refactored app.py file and separate helpers.py file

--- app.py
+++ app.py
@@ -1,214 +1,93 @@
-# General libraries
-import re
-import string
+# app.py
+
 import os
-import openai
 import logging
 from dotenv import load_dotenv
-import tiktoken
-
-load_dotenv()
-
-openai.api_key = os.environ["OPENAPI_KEY"]
-slack_key = os.environ["SLACK_BOT_TOKEN"]
-slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
-chat_gpt_model = 'gpt-3.5-turbo-0301'
-max_tokens = 3200
-encoding = tiktoken.encoding_for_model(chat_gpt_model)
-
-# Slack and Flask libraries
-from slack_sdk import WebClient
 from flask import Flask, request
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_sdk import WebClient
 
-# getting one week's worth on conversation history: used in the conversation history summary section
-import time
-import datetime as DT
-today = DT.date.today()
-week_ago = today - DT.timedelta(days=7)
-week_ago_unix = time.mktime(week_ago.timetuple())
+# Import helper functions
+from helpers import (
+    handle_summary_request,
+    handle_completion_request,
+    update_home_tab
+)
 
-# WebClient instantiates a client that can call API methods such as bot_ID and conversation history
+# Load environment variables
+load_dotenv()
+slack_key = os.environ["SLACK_BOT_TOKEN"]
+slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
+
+# Set up logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# Slack client setup
 client = WebClient(token=slack_key)
-bot_user_id = client.auth_test()
-bot_user_id = bot_user_id['user_id']
+bot_user_id = client.auth_test()["user_id"]
 
-# creating regex pattern for channel mention in direct mentiod
-summarization_request_regex = re.compile('<@U\w{10}> <#C\w{8}|\w{1,}>') # e.g. 'C04T04MPAJV'
-#  'text': '<@U04U6BU020K> <#CBH4YNBFF|lounge>'
-completion_request_regex = re.compile('<@U\w{10}> .*')
-# '<@U04QXFF5W9W> can you tell me'
-channel_name_regex = re.compile('C\w{8}') # e.g. 'CBH4YNBFF'
-
-# using bolt as message responder per guidance from Slack on how to avoid duplicate messages
-# https://github.com/slackapi/python-slack-sdk/issues/1164
-bolt_app = App(
-    token=slack_key,
-    signing_secret=slack_signing_secret)
-
+# Slack Bolt app setup
+bolt_app = App(token=slack_key, signing_secret=slack_signing_secret)
 
 @bolt_app.event("message")
-def handle_message_events(body, say, logger):
+def handle_message_events(body, say):
+    """
+    Handles incoming messages directed at the bot.
+    
+    Args:
+        body (dict): The payload containing event data from Slack.
+        say (function): Function to send a message back to the Slack channel.
+    """
     logger.info(body)
-    #print(body)
-    # handling DM's directed at the bot
-    if body['event']['channel_type'] == 'im' and body['event']['text'] == 'hi':
-         say('testing DM method')  # my user ID U04Q13P1GJJ
+    
+    channel_type = body['event']['channel_type']
+    text = body['event']['text']
+    
+    if channel_type == 'im' and text == 'hi':
+        say('Testing DM method')
+    
+    elif channel_type == 'im' and summarization_request_regex.match(text):
+        handle_summary_request(body, say, client, bot_user_id)
+    
+    elif channel_type == 'im' and completion_request_regex.match(text):
+        handle_completion_request(body, say, bot_user_id)
 
-    # handling channel summary request
-    elif body['event']['channel_type'] == 'im' and summarization_request_regex.match(body['event']['text']) != None:
-        completion_type = 'summarize'
-        channel_id = channel_name_regex.findall(body['event']['text'])[0]
-        say('Request received, please stand by')
-        #say('channel ' + channel_id + ' summary goes here')  # my user ID U04Q13P1GJJ
-        conversation_history = client.conversations_history(channel=channel_id, oldest=week_ago_unix)["messages"]
-        # print(conversation_history)
-        messages = [message["text"] for message in conversation_history if
-                    "text" in message and "bot_id" not in message]
-        messages = [message["text"] for message in conversation_history if
-                    "text" in message and message["user"] != bot_user_id]
-        length_of_messages = 'here are how many messages I found: ' + str(len(messages))
-        say(length_of_messages)
-
-        # creating one big message from all the conversations
-        text_for_prompt = "\n".join(messages)
-        char_length = 'I found this many characters in the message history ' + str(len(text_for_prompt))
-        say(char_length)
-        if len(text_for_prompt) > 12000:
-            say('This is a big summary request. Please be patient :)')
-        say(completion_request_handler(text_for_prompt, max_tokens, encoding, completion_type))
-
-    # handling general completion prompt
-    elif body['event']['channel_type'] == 'im' and completion_request_regex.match(body['event']['text']) != None:
-        completion_type = 'respond'
-        text_for_completion_prompt = body['event']['text']
-        text_for_completion_prompt = text_for_completion_prompt.replace(bot_user_id, '')
-        say('One second, processing your request ')
-        say(completion_request_handler(text_for_completion_prompt, max_tokens, encoding, completion_type))
-
-    return print(body)
-
-# handling bot app home views just for logging info
 @bolt_app.event("app_home_opened")
-def update_home_tab(client, event, logger):
-    logger.info(event)
-    client.views_publish(
-        user_id=event['user'],
-        view={
-            "type":"home",
-            "callback_id":"home_view",
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": '''*Welcome to the ChatGPT slack integration:tada: Go to the messages tab to interact.
-                            To use, open a direct message with the bot and do the following:
-                            1. To get a ChatGPT response, @mention the bot and write out your query.
-                            2. To do a channel summarization requestion, @mention the bot and the also #channel
-                                By default up to 7 days worth of messages will be retrieved.
-                                Make sure you add the bot to the channel first otherwise it won't work.  
-                                '''
-                    }
-                }],
-        }
-    )
+def app_home_opened_event(client, event):
+    """
+    Updates the app home tab when opened.
+    
+    Args:
+        client (WebClient): Slack WebClient instance to publish view updates.
+        event (dict): The event data containing user information.
+    """
+    update_home_tab(client, event)
 
-# summarizing mentions in a channel
-# @bolt_app.event("app_mention")
-# def gpt_completion(body, say, logger):
-#     # basic summary of whatever's contained in the chat request
-#     logger.info(body)
-#     #print(body)
-#
-#     # get # of conversions in the channel
-#     channel_id = (body['event']['channel'])
-#     conversation_history = client.conversations_history(channel=channel_id, oldest=week_ago_unix)["messages"]
-#     #print(conversation_history)
-#     messages = [message["text"] for message in conversation_history if "text" in message and "bot_id" not in message]
-#     messages = [message["text"] for message in conversation_history if
-#                 "text" in message and message["user"] != bot_user_id]
-#     length_of_messages = 'here are how many messages I found: ' + str(len(messages))
-#     say(length_of_messages)
-#     # client.chat_postMessage(channel=channel_id, text=length_of_messages)
-#
-#     # creating one big message from all the conversations
-#     text_for_prompt = "\n".join(messages)
-#     char_length = 'I found this many characters in the message history ' + str(len(text_for_prompt))
-#     say(char_length)
-#     if len(text_for_prompt) > 12000:
-#         say('This is a big summary request. Please be patient :)')
-#     # say('original text\n')
-#     # say(text_for_prompt)
-#     # say('====================================')
-#     # say('ChatGPT response to summary request')
-#     say(completion_request_handler(text_for_prompt, max_tokens, encoding))
-
-
+# Flask app setup
 app = Flask(__name__)
-
 handler = SlackRequestHandler(bolt_app)
 
 @app.route("/slack/events", methods=["POST"])
 def slack_events():
+    """
+    Handles Slack events forwarded to this endpoint by Slack's Events API.
+    
+    Returns:
+        Response: The HTTP response returned to Slack.
+    """
     return handler.handle(request)
-
 
 @app.route('/', methods=['GET'])
 def healthcheck():
+    """
+    Healthcheck endpoint to verify the service is running.
+    
+    Returns:
+        str: A simple 'OK' message.
+    """
     return 'OK'
-
-logger = logging.getLogger(__name__)
-
-
-def completion_request_handler(text_for_prompt, max_tokens, encoding, completion_type):
-    completion_type = completion_type
-    prompt_tokens = encoding.encode(text_for_prompt)
-    if len(prompt_tokens) < max_tokens:
-        return get_summary_chatgpt(text_for_prompt, completion_type)
-    else:
-        responses = []
-        return recursive_completion(prompt_tokens, max_tokens, encoding, responses, )
-
-
-def get_summary_chatgpt(text_for_prompt, completion_type):
-    completion_type = completion_type
-    response = openai.ChatCompletion.create(
-        model=chat_gpt_model,
-        messages=[
-            {"role": "system", "content": '''You are an assistant in Slack that provides content completion
-                                                        and content summarization upon user request.'''},
-            {"role": "user", "content": 'Please ' + completion_type + ''' the following messages with a rich level of detail 
-                                            and be as clear as possible: ''' + "###\n" + text_for_prompt},
-        ]
-    )
-    print(response)
-    return response.choices[0].message['content']
-
-def recursive_completion(prompt_tokens, max_tokens, encoding, responses, completion_type):
-    completion_type = completion_type
-    i = 0
-    while len(prompt_tokens) > 0:
-        print('prompt token length is ' + str(len(prompt_tokens)))
-        # gets the 'head' tokens of the token bunch
-        chunk = encoding.decode(prompt_tokens[:min(max_tokens, len(prompt_tokens))])
-        print('now on chunk number: ' + str(i))
-        i += 1
-        responses.append(get_summary_chatgpt(chunk, completion_type))
-        # slices off the 'head' and continues through
-        prompt_tokens = prompt_tokens[min(max_tokens, len(prompt_tokens)):len(prompt_tokens)]
-        print('prompt token length is ' + str(len(prompt_tokens)))
-        if len(prompt_tokens) < max_tokens:
-            print('completing recursive function')
-            responses.append(get_summary_chatgpt(encoding.decode(prompt_tokens), completion_type))
-            prompt_tokens = prompt_tokens[len(prompt_tokens):len(prompt_tokens)]
-            print('prompt token length is ' + str(len(prompt_tokens)))
-            break
-    return '\n'.join(responses)
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ helpers.py
@@ -0,0 +1,162 @@
+# helpers.py
+
+import os
+import time
+import datetime as DT
+import re
+import openai
+import tiktoken
+from slack_sdk import WebClient
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+openai.api_key = os.environ["OPENAPI_KEY"]
+CHAT_GPT_MODEL = 'gpt-3.5-turbo-0301'
+MAX_TOKENS = 3200
+ENCODING = tiktoken.encoding_for_model(CHAT_GPT_MODEL)
+
+# Date calculations for conversation history
+today = DT.date.today()
+week_ago = today - DT.timedelta(days=7)
+week_ago_unix = time.mktime(week_ago.timetuple())
+
+# Regex patterns
+summarization_request_regex = re.compile(r'<@U\w{10}> <#C\w{8}|\w{1,}>')
+completion_request_regex = re.compile(r'<@U\w{10}> .*')
+channel_name_regex = re.compile(r'C\w{8}')
+
+def handle_summary_request(body, say, client, bot_user_id):
+    """
+    Handles summarization requests for a Slack channel.
+
+    Args:
+        body (dict): The payload containing event data from Slack.
+        say (function): Function to send a message back to the Slack channel.
+        client (WebClient): Slack WebClient instance to fetch conversation history.
+        bot_user_id (str): The Slack bot user ID to filter out bot messages.
+    """
+    say('Request received, please stand by')
+    channel_id = channel_name_regex.findall(body['event']['text'])[0]
+    conversation_history = client.conversations_history(channel=channel_id, oldest=week_ago_unix)["messages"]
+    
+    messages = [message["text"] for message in conversation_history if "text" in message and message.get("user") != bot_user_id]
+    say(f'Found {len(messages)} messages in the channel.')
+    
+    text_for_prompt = "\n".join(messages)
+    if len(text_for_prompt) > 12000:
+        say('This is a big summary request. Please be patient :)')
+    
+    response = completion_request_handler(text_for_prompt, MAX_TOKENS, ENCODING, 'summarize')
+    say(response)
+
+def handle_completion_request(body, say, bot_user_id):
+    """
+    Handles general completion requests.
+
+    Args:
+        body (dict): The payload containing event data from Slack.
+        say (function): Function to send a message back to the Slack channel.
+        bot_user_id (str): The Slack bot user ID to filter out bot mentions.
+    """
+    text_for_completion_prompt = body['event']['text'].replace(bot_user_id, '')
+    say('One second, processing your request')
+    
+    response = completion_request_handler(text_for_completion_prompt, MAX_TOKENS, ENCODING, 'respond')
+    say(response)
+
+def update_home_tab(client, event):
+    """
+    Updates the Slack app's home tab when opened.
+
+    Args:
+        client (WebClient): Slack WebClient instance to publish view updates.
+        event (dict): The event data containing user information.
+    """
+    client.views_publish(
+        user_id=event['user'],
+        view={
+            "type": "home",
+            "callback_id": "home_view",
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": ("*Welcome to the ChatGPT Slack integration :tada:*\n"
+                                 "To use, open a direct message with the bot and follow these steps:\n"
+                                 "1. To get a ChatGPT response, @mention the bot and write your query.\n"
+                                 "2. To request a channel summary, @mention the bot and include #channel."
+                                 "Up to 7 days worth of messages will be retrieved. Ensure the bot is in the channel.")
+                    }
+                }
+            ]
+        }
+    )
+
+def completion_request_handler(text_for_prompt, max_tokens, encoding, completion_type):
+    """
+    Handles the completion request for text prompts to be processed by ChatGPT.
+
+    Args:
+        text_for_prompt (str): The text to be processed by ChatGPT.
+        max_tokens (int): The maximum number of tokens for ChatGPT to process.
+        encoding (tiktoken.Encoding): Encoding instance to handle tokenization.
+        completion_type (str): The type of completion requested (e.g., 'summarize' or 'respond').
+
+    Returns:
+        str: The response from ChatGPT.
+    """
+    prompt_tokens = encoding.encode(text_for_prompt)
+    
+    if len(prompt_tokens) < max_tokens:
+        return get_summary_chatgpt(text_for_prompt, completion_type)
+    else:
+        responses = []
+        return recursive_completion(prompt_tokens, max_tokens, encoding, responses, completion_type)
+
+def get_summary_chatgpt(text_for_prompt, completion_type):
+    """
+    Fetches the summary or response from ChatGPT based on the given text prompt.
+
+    Args:
+        text_for_prompt (str): The text to be processed by ChatGPT.
+        completion_type (str): The type of completion requested (e.g., 'summarize' or 'respond').
+
+    Returns:
+        str: The generated content from ChatGPT.
+    """
+    response = openai.ChatCompletion.create(
+        model=CHAT_GPT_MODEL,
+        messages=[
+            {"role": "system", "content": ("You are an assistant in Slack that provides content completion "
+                                           "and content summarization upon user request.")},
+            {"role": "user", "content": f'Please {completion_type} the following messages with a rich level of detail and be as clear as possible: ###\n{text_for_prompt}'}
+        ]
+    )
+    return response.choices[0].message['content']
+
+def recursive_completion(prompt_tokens, max_tokens, encoding, responses, completion_type):
+    """
+    Handles large prompt tokens recursively to fit within the token limit of ChatGPT.
+
+    Args:
+        prompt_tokens (list): The list of tokenized prompts to be processed by ChatGPT.
+        max_tokens (int): The maximum number of tokens for each chunk to be processed.
+        encoding (tiktoken.Encoding): Encoding instance to handle tokenization and decoding.
+        responses (list): The list to accumulate responses from ChatGPT.
+        completion_type (str): The type of completion requested (e.g., 'summarize' or 'respond').
+
+    Returns:
+        str: The combined response from ChatGPT after processing all token chunks.
+    """
+    while len(prompt_tokens) > 0:
+        chunk = encoding.decode(prompt_tokens[:min(max_tokens, len(prompt_tokens))])
+        responses.append(get_summary_chatgpt(chunk, completion_type))
+        
+        prompt_tokens = prompt_tokens[min(max_tokens, len(prompt_tokens)):]
+        if len(prompt_tokens) < max_tokens:
+            responses.append(get_summary_chatgpt(encoding.decode(prompt_tokens), completion_type))
+            break
+    
+    return '\n'.join(responses)
